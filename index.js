f99dd1456773f6ec582038a5dab1559c27a20547
stephensanchez05 <stephensanchez05@gmail.com>
2020-07-01 16:14:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(source, fun) {
+    const map1 = source.map(x => fun(x));
+    return map1
+}
+
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
